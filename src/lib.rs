124c5ec139a09f5c417b0b7e3cec1e038a2350e7
jeremymarch <jmarch@gradcenter.cuny.edu>
2022-08-31 00:09:55 -0400

    add param to next_form for previous answer and return true/false/none in tuple

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 extern crate hoplite_verbs_rs;
 use hoplite_verbs_rs::*;
+use rustunicodetests::hgk_compare;
 use rand::seq::SliceRandom;
 //use rand::Rng;
 use rand::thread_rng;
@@ -23,7 +24,8 @@ use std::sync::Arc; //https://stackoverflow.com/questions/41770184/arc-reference
 // }
 
 pub trait FormChooser {
-    fn next_form(&mut self) -> Result<HcGreekVerbForm, &str>;
+    fn next_form(&mut self, prev_answer:Option<&str>) -> Result<(HcGreekVerbForm, Option<bool>), &str>;
+    fn set_reps_per_verb(&mut self, reps:u8);
 }
 
 // pub struct RandomFormChooser<'a> {
@@ -37,14 +39,14 @@ pub struct RandomFormChooser {
     pub unit: u32,
     pub history: Vec<HcGreekVerbForm>,
     pub params_to_change: u8,
-    pub reps_per_verb: u8,
+    reps_per_verb: u8,
     pub persons: Vec<HcPerson>,
     pub numbers: Vec<HcNumber>,
     pub tenses: Vec<HcTense>,
     pub moods: Vec<HcMood>,
     pub voices: Vec<HcVoice>,
     verb_counter: u8,
-    verb_idx:usize,
+    verb_idx: usize,
 }
 
 pub fn init_random_form_chooser(path:&str, unit: u32) -> RandomFormChooser {
@@ -58,24 +60,35 @@ pub fn init_random_form_chooser(path:&str, unit: u32) -> RandomFormChooser {
         }
     }
     verbs.shuffle(&mut thread_rng());
-    for (idx,a) in verbs.iter().enumerate() {
-        println!("v{}: {}", idx, a.pps[0]);
-    }
+    // for (idx,a) in verbs.iter().enumerate() {
+    //     println!("v{}: {}", idx, a.pps[0]);
+    // }
 
+    //default params available, these can be set
     let persons = vec![HcPerson::First, HcPerson::Second, HcPerson::Third];
     let numbers = vec![HcNumber::Singular, HcNumber::Plural];
     let tenses = vec![HcTense::Present, HcTense::Imperfect, HcTense::Future, HcTense::Aorist, HcTense::Perfect, HcTense::Pluperfect];
-    let moods = vec![HcMood::Indicative, HcMood::Subjunctive,HcMood::Optative,HcMood::Imperative];
-    let voices = vec![HcVoice::Active,HcVoice::Middle,HcVoice::Passive];
+    let moods = vec![HcMood::Indicative, HcMood::Subjunctive, HcMood::Optative, HcMood::Imperative];
+    let voices = vec![HcVoice::Active, HcVoice::Middle, HcVoice::Passive];
 
     RandomFormChooser {verbs, unit, history: vec![], params_to_change: 2, reps_per_verb: 4,  persons, numbers, tenses, moods, voices, verb_counter: 0, verb_idx: 0 }
 }
 
 impl FormChooser for RandomFormChooser {
-    fn next_form(&mut self) -> Result<HcGreekVerbForm, &str> {
+    fn set_reps_per_verb(&mut self, reps:u8) {
+        if reps > 0 && reps < 10 {
+            self.reps_per_verb = reps;
+        }
+        else {
+            self.reps_per_verb = 4;
+        }
+    }
+
+    fn next_form(&mut self, prev_answer:Option<&str>) -> Result<(HcGreekVerbForm, Option<bool>), &str> {
         let mut count = 0;
         let mut a:HcGreekVerbForm;
         let mut found = false;
+        let mut is_correct:Option<bool> = None;
 
         if self.verbs.len() < 1 {
             return Err("no verbs");
@@ -95,6 +108,20 @@ impl FormChooser for RandomFormChooser {
         }
 
         self.verb_counter += 1;
+
+        if self.history.len() > 0 && prev_answer.is_some() {
+            //check verb
+            let prev_form = self.history.last().unwrap();
+            let prev_s = prev_form.get_form(false).unwrap().last().unwrap().form.to_string();
+            if hgk_compare(&prev_s, prev_answer.unwrap(), 0) == 0 {
+                println!("same");
+                is_correct = Some(true);
+            }
+            else {
+                println!("not same");
+                is_correct = Some(false);
+            }
+        }
 
         loop {
             count += 1;
@@ -127,13 +154,12 @@ impl FormChooser for RandomFormChooser {
             else {
                 //println!("\t\tNope Form: {} {:?} {:?} {:?} {:?} {:?}", a.verb.pps[0], a.person, a.number, a.tense, a.mood, a.voice);
             }
-            
         }
 
         if found {
             //if let Some(f) = self.history.last().unwrap() {
                 //println!("\tForm: {} {:?} {:?} {:?} {:?} {:?}", self.history.last().unwrap().verb.pps[0], self.history.last().unwrap().person, self.history.last().unwrap().number, self.history.last().unwrap().tense, self.history.last().unwrap().mood, self.history.last().unwrap().voice);
-                return Ok(self.history.last().unwrap().clone());
+                return Ok((self.history.last().unwrap().clone(), is_correct));
             //}
         }
 
@@ -147,16 +173,18 @@ mod tests {
     #[test]
     fn it_works() {        
         let mut chooser = init_random_form_chooser("../hoplite_verbs_rs/testdata/pp.txt", 20);
+        chooser.set_reps_per_verb(6);
         // chooser.persons = vec![HcPerson::First];
         // chooser.numbers = vec![HcNumber::Singular];
 
         for _ in 0..=10016 {
-            let mut vf = chooser.next_form().unwrap();
-            let mut vfs = vf.get_form(false).unwrap().last().unwrap().form.to_string();
-            println!("{:?} {:?} {:?} {:?} {:?} \t\t\t- {}", vf.person, vf.number, vf.tense, vf.mood, vf.voice, vfs);
+            let mut vf = chooser.next_form(None).unwrap();
+            let is_correct = vf.1;
+            let mut vfs = vf.0.get_form(false).unwrap().last().unwrap().form.to_string();
+            println!("{:?} {:?} {:?} {:?} {:?} \t\t\t- {} {:?}", vf.0.person, vf.0.number, vf.0.tense, vf.0.mood, vf.0.voice, vfs, is_correct);
         }
 
-        assert_eq!(chooser.next_form().unwrap().get_form(false).unwrap().last().unwrap().form.to_string(), String::from("ἔλῡσα"));
-        assert_ne!(chooser.next_form().unwrap().get_form(false).unwrap().last().unwrap().form.to_string(), String::from("ἔλῡσα"));
+        assert_eq!(chooser.next_form(None).unwrap().0.get_form(false).unwrap().last().unwrap().form.to_string(), String::from("ἔλῡσα"));
+        assert_ne!(chooser.next_form(None).unwrap().0.get_form(false).unwrap().last().unwrap().form.to_string(), String::from("ἔλῡσα"));
     }
 }
